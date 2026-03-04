bde22e78fd13dbb482e4f725791e388d47116f59
Naveed Ahmed Syed <syednaveed2006@icloud.com>
2026-03-03 23:44:52 -0500

    feat: Mac Scroll configuration added

---
+++ app/components/SmoothScroll.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useEffect } from "react";
+import Lenis from "lenis";
+
+export default function SmoothScroll({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const lenis = new Lenis({
+  duration: 0.5,
+  easing: (t) => 1 - Math.pow(1 - t, 3),
+  smoothWheel: true,
+  wheelMultiplier: 1.5, 
+});
+
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => lenis.destroy();
+  }, []);
+
+  return <>{children}</>;
+}
